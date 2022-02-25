3f3db61ee935618423cdee2cfd000650f10655b6
Connor <90352499+Connor-vb@users.noreply.github.com>
2022-02-24 21:48:35 -0800

    added event listener

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -11,3 +11,13 @@ var urlsToCache = [
   "/icons/icon-512x512.png"
 ];
 
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
